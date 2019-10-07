44b32c4764d21a7458d0235af559b42697a75f97
Neoko <neo33.pro@gmail.com>
2019-10-07 18:36:05 +0200

    keyboard bindings

---
+++ keys.py
@@ -0,0 +1,8 @@
+import curses
+
+pause    = (127)
+down     = (curses.KEY_DOWN,   ord('s'))
+up       = (curses.KEY_UP,     ord('z'))
+right    = (curses.KEY_RIGHT,  ord('d'))
+left     = (curses.KEY_LEFT,   ord('q'))
+interact = (curses.ENTER,      ord('e'))
