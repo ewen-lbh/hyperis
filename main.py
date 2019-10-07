0e2708f89ffdc2f6e986af42df2f12d8d177e092
Neoko <neo33.pro@gmail.com>
2019-10-07 18:36:20 +0200

    (WIP) mainloop

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import click
+import curses
 import termcolor
 import pyfiglet
+from keys import *
+
+
+window_size = (80, 80)  # (cols, rows)
+
+win = curses.newwin(*window_size)
+curses.noecho()
+curses.cbreak()
+win.keypad(1)
+curses.curs_set(0)
+
+win.clear()
+win.border()
+win.nodelay(1)
+
+while True:
+    key = win.getch()
+    if key in keys.pause:
+        #TODO: pause menu
+        break
+    elif key in keys.down:
+        player.move('down')
+    elif key == curses.KEY_UP
